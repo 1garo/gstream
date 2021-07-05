2fd3f8473c9710b2e3cdc3b46bfec21bd6aea89a
1garo <alevardai427@gmail.com>
2021-07-05 13:37:59 -0300

    feat: create api to respond video chunks

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	http.Handle("/", handlers())
+	http.ListenAndServe(":8000", nil)
+}
+
+func handlers() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", indexPage).Methods("GET")
+	router.HandleFunc("/media/{mId:[0-9]+}/stream/", streamHandler).Methods("GET")
+	router.HandleFunc("/media/{mId:[0-9]+}/stream/{segName:index[0-9]+.ts}", streamHandler).Methods("GET")
+	return router
+}
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func streamHandler(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	mId, err := strconv.Atoi(vars["mId"])
+	fmt.Printf("%d\n", mId)
+	if err != nil {
+		fmt.Print("entrou not found\n")
+		response.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	segName, ok := vars["segName"]
+	if !ok {
+		fmt.Print("entrou aqui\n")
+		mediaBase := getMediaBase(mId)
+		m3u8Name := "index.m3u8"
+		serveHlsM3u8(response, request, mediaBase, m3u8Name)
+	} else {
+		fmt.Print("entrou 21\n")
+		mediaBase := getMediaBase(mId)
+		serveHlsTs(response, request, mediaBase, segName)
+	}
+}
+
+func getMediaBase(mId int) string {
+	mediaRoot := "assets/media"
+	return fmt.Sprintf("%s/%d", mediaRoot, mId)
+}
+
+func serveHlsM3u8(w http.ResponseWriter, r *http.Request, mediaBase, m3u8Name string) {
+	mediaFile := fmt.Sprintf("%s/hls/%s", mediaBase, m3u8Name)
+	fmt.Printf("%s\n", mediaFile)
+	http.ServeFile(w, r, mediaFile)
+	w.Header().Set("Content-Type", "application/x-mpegURL")
+}
+
+func serveHlsTs(w http.ResponseWriter, r *http.Request, mediaBase, segName string) {
+	mediaFile := fmt.Sprintf("%s/hls/%s", mediaBase, segName)
+	http.ServeFile(w, r, mediaFile)
+	w.Header().Set("Content-Type", "video/MP2T")
+}
